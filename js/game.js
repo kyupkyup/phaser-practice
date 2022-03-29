f6ab21a7a9d684728a2b3abf4c14e4d0a965b5ba
kyung yup <kyungyup@kyungui-MacBookPro.local>
2022-03-29 15:11:47 +0900

    image route fix

--- js/game.js
+++ js/game.js
@@ -1,11 +1,11 @@
 class Main {
   preload() {
-    this.load.image('background', '../assets/background.png')
-    this.load.image('player', '../assets/player.png')
-    this.load.image('wallV', '../assets/wallVertical.png')
-    this.load.image('wallH', '../assets/wallHorizontal.png')
-    this.load.image('coin', '../assets/coin.png')
-    this.load.image('enemy', '../assets/enemy.png')
+    this.load.image('background', './assets/background.png')
+    this.load.image('player', './assets/player.png')
+    this.load.image('wallV', './assets/wallVertical.png')
+    this.load.image('wallH', './assets/wallHorizontal.png')
+    this.load.image('coin', './assets/coin.png')
+    this.load.image('enemy', './assets/enemy.png')
   }
 
   createWorld() {
